072dd72c473114f38b9a88b7df8b8115bb44445f
Mandy Liu <manling512@gmail.com>
2023-11-23 19:22:58 -0700

    Implement HTTP basic auth as a way of securing the admin/products resource

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module New
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Add the following line to enable full Rails functionality (not API-only mode)
+    config.api_only = false
   end
 end
